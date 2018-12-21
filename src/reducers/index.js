305f45dfdc1f60126067d578d7635fd4b25aaa4c
Hieu Tran <hmtran.ae@gmail.com>
2018-12-20 21:50:24 -0500

    finished app

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -3,8 +3,8 @@ import { combineReducers } from 'redux';
 const songsReducer = () => {
     return [
         { title: 'No Scrubs', duration: '4:05' },
-        { title: 'Macarena', duraton: '2:30' },
-        { title: 'ALl star', duration: '3:15' },
+        { title: 'Macarena', duration: '2:30' },
+        { title: 'All star', duration: '3:15' },
         { title: 'I want it That Way', duration: '1:45' }
     ];
 };
